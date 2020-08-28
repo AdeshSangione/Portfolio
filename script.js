2939a56b9b8e30121a8747db3379dbbf6c6e719c
Adesh <adesh@ualberta.ca>
2020-08-27 19:32:18 -0600

    First Commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// Functionality for burger menu
+
+function pageRedirectARVP(){
+  window.location.href = "ARVP.html";
+}
+
+function pageRedirectLED(){
+  window.location.href = "LED.html";
+}
+
+function pageRedirectBLE(){
+  window.location.href = "BLE.html";
+}
+
+function pageRedirectARVPWebsite(){
+  window.location.href = "https://arvp.org/";
+}
+
+function pageRedirectML(){
+  window.location.href = "ARVP.html";
+}
+
+function pageRedirectHome(){
+  window.location.href = "Portfolio.html";
+}
+
+function myFunction() {
+  var x = document.getElementById("navbarToggle");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
